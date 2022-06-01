c4c10f26f5e65c69dcc3782b8da59ffa2dad9dcb
majf <734663446@qq.com>
2022-06-01 17:53:24 +0800

    添加readme

---
+++ alex-miaosha/src/main/java/com/alex/blog/miaosha/MiaoshaMain.java
@@ -0,0 +1,18 @@
+package com.alex.blog.miaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *description:  秒杀系统启动类
+ *author:       alex
+ *createDate:   2022/6/1 16:34
+ *version:      1.0.0
+ */
+@SpringBootApplication
+public class MiaoshaMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoshaMain.class, args);
+    }
+}
